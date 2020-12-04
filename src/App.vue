258b9610108319feb7ae30736d90693989a2cfdc
Luis Antonio Parrado <luisprmat@gmail.com>
2020-12-04 12:47:20 -0500

    Add API news

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,113 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <nav class="navbar navbar-expand-md navbar-light bg-light mb-3">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler d-lg-none"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collapsibleNavId"
+        aria-controls="collapsibleNavId"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavId">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <h2 class="text-center mb-4">Noticias de Colombia</h2>
+      <div class="col-md-8 mx-auto mb-3">
+        <div class="d-flex justify-content-between align-items-baseline">
+          <label for="category" class="mb-2 mr-sm-2">Categoría: </label>
+          <select
+            id="category"
+            class="custom-select mb-2 mr-sm-2"
+            v-model="category"
+            @change="loadNews"
+          >
+            <option
+              v-for="category in categories"
+              :key="category.value"
+              :value="category.value"
+              >{{ category.name }}</option
+            >
+          </select>
+        </div>
+      </div>
+      <div class="row mx-auto mb-3" v-if="news.length">
+        <div class="col-md-3"><news-panel :item="news[0]" /></div>
+        <div class="col-md-3"><news-panel :item="news[1]" /></div>
+        <div class="col-md-3"><news-panel :item="news[2]" /></div>
+        <div class="col-md-3"><news-panel :item="news[3]" /></div>
+      </div>
+      <div>
+        <!-- <pre>
+          {{ $data }}
+        </pre> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import "bootstrap";
+import NewsPanel from "./components/NewsPanel";
+const axios = require("axios");
+axios.defaults.headers = {
+  "X-Api-Key": process.env.VUE_APP_API_NEWS_KEY
+};
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    NewsPanel
+  },
+  data() {
+    return {
+      categories: [
+        { value: "science", name: "Ciencia" },
+        { value: "sports", name: "Deportes" },
+        { value: "entertainment", name: "Entretenimiento" },
+        { value: "general", name: "General" },
+        { value: "business", name: "Negocios" },
+        { value: "health", name: "Salud" },
+        { value: "technology", name: "Tecnología" }
+      ],
+      category: "technology",
+      news: []
+    };
+  },
+  mounted() {
+    this.loadNews();
+  },
+  methods: {
+    loadNews() {
+      // const apiKey = process.env.VUE_APP_API_NEWS_KEY;
+      // let url = `https://newsapi.org/v2/top-headlines?country=co&category=${this.category}&apiKey=${apiKey}`;
+      let url = `https://newsapi.org/v2/top-headlines?country=co&category=${this.category}`;
+      axios
+        .get(url)
+        .then(response => {
+          this.news = response.data.articles;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bootstrap/dist/css/bootstrap.css";
 </style>
---
+++ src/components/NewsPanel.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card">
+    <img
+      class="card-img-top"
+      :src="item.urlToImage"
+      :alt="item.title"
+      v-if="item.urlToImage"
+    />
+    <div class="card-body">
+      <h4 class="card-title">{{ item.title }}</h4>
+      <p class="card-text">{{ item.description }}</p>
+      <small class="card-text text-secondary font-italic text-small">{{
+        dateFormat
+      }}</small>
+      <hr />
+      <a :href="item.url" class="btn btn-info">Ver noticia completa ...</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: "NewsPanel",
+  props: ["item"],
+  computed: {
+    dateFormat() {
+      moment.locale("es");
+      return moment(this.item.publishedAt).format("MMMM D YYYY, h:mm:ss a");
+    }
+  }
+};
+</script>
